bbdee8b96960c456a10143c2ebadade4cf70546a
Lenard <energeticbowlofrice@gmail.com>
2024-03-18 15:36:30 +0800

    Fetched all todo_item from the database and display them

--- index.php
+++ index.php
@@ -9,9 +9,27 @@
 
 	<form method="POST" action="addtodo.php">
 		<label for="todo_item">Todo</label>
-		<input type="text" name="todo_item" id="todo_item">
+		<input type="text" name="todo_item" id="todo_item" required>
 		<button type="submit">Add Todo Item</button>
 	</form>
+	
+	<?php
 
+	$conn = new PDO("sqlite:file:todo.db?mode=rwc");
+	
+	$stmt = $conn->prepare("SELECT todo_item FROM todo");
+	$stmt->execute();
+	$rows = $stmt->fetchall();
+
+	echo "<ul>";
+	for ($i = 0; $i < count($rows); $i++) {
+		echo "<li>{$rows[$i][0]}</li>";
+	}
+	echo "</ul>";
+
+	$conn = null;
+	$stmt = null;
+
+	?>
 </body>
 </html>
