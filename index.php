d7722e043341d04e32449ba12be76914226471bf
Lenard <energeticbowlofrice@gmail.com>
2024-03-23 21:49:14 +0800

    Added css styling

--- index.php
+++ index.php
@@ -6,36 +6,41 @@
 	<title>Todo Application in PHP</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body class="very-dark-blue-bg">
+	<header class="hero">
+		<h1 class="big-text">TODO</h1>
+	</header>
+	<main>
+		<form method="POST" action="/addtodo.php">
+			<input class="very-dark-desaturated-blue-bg small-rectangle-container" type="text" name="todo_item" id="todo_item" placeholder="Create a new todo..." autofocus required>
+			<button type="submit" hidden>Add Todo Item</button>
+		</form>
+		
+		<?php
+		$conn = new PDO("sqlite:file:todo.db?mode=rwc");
 
-	<form method="POST" action="/addtodo.php">
-		<input type="text" name="todo_item" id="todo_item" autofocus required>
-		<button type="submit" hidden>Add Todo Item</button>
-	</form>
-	
-	<?php
-	$conn = new PDO("sqlite:file:todo.db?mode=rwc");
+		$conn->exec("CREATE TABLE IF NOT EXISTS todo(todo_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, todo_item TEXT NOT NULL)");
+		
+		$stmt = $conn->prepare("SELECT * FROM todo");
+		$stmt->execute();
+		$rows = $stmt->fetchall();
 
-	$conn->exec("CREATE TABLE IF NOT EXISTS todo(todo_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, todo_item TEXT NOT NULL)");
-	
-	$stmt = $conn->prepare("SELECT * FROM todo");
-	$stmt->execute();
-	$rows = $stmt->fetchall();
+		echo "<ul>";
+		for ($i = 0; $i < count($rows); $i++) {
+			echo "<li value={$rows[$i]["todo_id"]} class='very-dark-desaturated-blue-bg small-rectangle-container'>{$rows[$i]["todo_item"]}</li>";
+		}
+		echo "</ul>";
 
-	echo "<ul>";
-	for ($i = 0; $i < count($rows); $i++) {
-		echo "<li value={$rows[$i]["todo_id"]}>{$rows[$i]["todo_item"]}</li>";
-	}
-	echo "</ul>";
+		$conn = null;
+		$stmt = null;
+		?>
 
-	$conn = null;
-	$stmt = null;
-	?>
-
-	<button id="showActiveBtn">Active</button>
-	<button id="showCompletedBtn">Completed</button>
-	<button id="deleteTodosBtn">Clear Completed</button>
-
+		<div class="very-dark-desaturated-blue-bg small-rectangle-container">
+			<button id="showActiveBtn" class="very-dark-desaturated-blue-bg">Active</button>
+			<button id="showCompletedBtn" class="very-dark-desaturated-blue-bg">Completed</button>
+			<button id="deleteTodosBtn" class="very-dark-desaturated-blue-bg">Clear Completed</button>
+		</div>
+	</main>
 	<script src="index.js"></script>
 </body>
 </html>
